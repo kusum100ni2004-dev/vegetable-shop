aa5045c4fa9e6151701a9342e5817acc61bf2ea3
Kusum <kusum100ni2004@gmail.com>
2026-08-09 17:02:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config(); 
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -9,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-require("dotenv").config(); 
-mongoose.connect("process.env.MONGO_URI")
+
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
